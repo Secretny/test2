314ce5d3ece4f1f8e22db3ff64301cfa59e6ec96
Szymon <ataszymon@gmail.com>
2022-01-07 09:00:55 +0100

    commit dodanie get /data

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get("/", function (req, res) {
     res.send("<h1>Moja aplikacja na heroku - zmiana pliku</h1><h2>Szymon Migas gr II</h2>")
 })
 
+app.get("/data", function (req, res) {
+    const data = { imie: "Szymon", nazwisko: "Migas", klasa: "3i1", grupa: "2" }
+    res.send(data)
+})
+
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
